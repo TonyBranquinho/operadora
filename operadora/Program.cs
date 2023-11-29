8d3015e5ea3dae6e816b2cae7d18490da3c97eec
Tony <antony.branquinho@gmail.com>
2023-11-29 16:01:52 -0300

    facil

---
+++ operadora/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operadora {
+    class Program {
+        static void Main(string[] args) {
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            double total, excedente, minutos, subTotal, franquia;
+
+            Console.Write("Digite a quantidade de minutos: ");
+            minutos = double.Parse(Console.ReadLine());
+
+            franquia = 100;
+            if (minutos < franquia) {
+                Console.WriteLine("Valor a pagar: R$50.00");
+            }
+            else {
+                excedente = minutos - franquia;
+                subTotal = excedente * 2;
+                total = subTotal + 50.00;
+                Console.WriteLine("Valor a pagar: " + total.ToString("F2"));
+            }
+
+        }
+    }
+}
